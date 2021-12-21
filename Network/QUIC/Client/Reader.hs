7764092428b158bbd59c5550c6953568d0c61ebd
Kazu Yamamoto <kazu@iij.ad.jp>
2021-12-21 12:39:21 +0900

    fix

--- Network/QUIC/Client/Reader.hs
+++ Network/QUIC/Client/Reader.hs
@@ -39,8 +39,8 @@ readerClient s0 conn = handleLogUnit logAction $
               Nothing  ->     NSB.recv     s0 maximumUdpPayloadSize
               Just sa0 -> do
                   putStrLn "XXXXX"
-                  -- getSocketName s0 >>= print
-                  -- getPeerName s0 >>= print
+                  (getSocketName s0 >>= print) `E.catchAny` \_ -> putStrLn "getSocketName"
+                  (getPeerName s0 >>= print) `E.catchAny` \_ -> putStrLn "getPeerName"
                   putStrLn "recvFrom..."
                   (bs, sa) <- NSB.recvFrom s0 maximumUdpPayloadSize
                   putStrLn "recvFrom...done"
